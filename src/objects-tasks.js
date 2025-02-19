ae38249e5640ab3bd2a8bdecb5aac436d66807c1
Alina M <crlin.m@gmail.com>
2025-02-19 13:44:40 +0500

    feat: implement makeImmutable, makeWord functions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -127,8 +127,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -141,8 +141,15 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  return Object.entries(lettersObject)
+    .reduce((acc, [key, value]) => {
+      value.forEach((ind) => {
+        acc[ind] = key;
+      });
+      return acc;
+    }, [])
+    .join('');
 }
 
 /**
